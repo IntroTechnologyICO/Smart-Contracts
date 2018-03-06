ba13e7477717f3a5e5165c395b7b9229129cb9fb
Egor Zaremba <EgorZaremba@gmail.com>
2018-03-06 09:40:20 +0300

    burnable add + erc20 requires

--- IntroToken.sol
+++ IntroToken.sol
@@ -217,6 +217,27 @@ contract StandardToken is ERC20, BasicToken {
 
 }
 
+contract BurnableToken is BasicToken {
+
+  event Burn(address indexed burner, uint256 value);
+
+  /**
+   * @dev Burns a specific amount of tokens.
+   * @param _value The amount of token to be burned.
+   */
+  function burn(uint256 _value) public {
+    require(_value <= balances[msg.sender]);
+    // no need to require value <= totalSupply, since that would imply the
+    // sender's balance is greater than the totalSupply, which *should* be an assertion failure
+
+    address burner = msg.sender;
+    balances[burner] = balances[burner].sub(_value);
+    totalSupply = totalSupply.sub(_value);
+    Burn(burner, _value);
+    Transfer(burner, address(0), _value);
+  }
+}
+
 contract IntroCoin is StandardToken {
 
   string public name = "Intro Coin";
